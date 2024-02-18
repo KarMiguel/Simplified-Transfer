57c35d712bab3ba7aa51f3ab036ea6cfe4bfe74b
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-02-18 10:19:48 -0300

    feat: create Transaction entity

---
+++ src/main/java/org/example/domain/transaction/Transaction.java
@@ -0,0 +1,29 @@
+package org.example.domain.transaction;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.domain.user.User;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+public class Transaction {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private Double amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+}
