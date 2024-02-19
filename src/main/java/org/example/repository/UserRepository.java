aa4d098dd7645e511349847a41bb7d742216f598
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-02-19 09:10:41 -0300

    feat: mudance repository user

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -2,12 +2,13 @@ package org.example.repository;
 
 import org.example.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    Optional<User> findUserByDocument (String document);
-    Optional<User> findUserById (Long id);
+    Optional<User> findById(Long id);
 
 }
